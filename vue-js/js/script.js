42050f8929de909672af934a4554007e081c6178
Cezar <cezarlantosgmail.com>
2022-01-17 17:45:03 +0100

    terminato

--- vue-js/js/script.js
+++ vue-js/js/script.js
@@ -16,18 +16,15 @@ new Vue ({
             },
         ],
         newTask: '',
-        // done: false,
     },
     methods: {
         addTask: function () {
-            this.tasks.push(this.newTask),
+            let mission = {text: this.newTask, done: false}
+            this.tasks.push(mission)
             this.newTask = ''
         },
         removeTask: function (i) {
            this.tasks.splice(i, 1) 
         },
-        // taskDone: function () {
-        //     this.done = !this.done;
-        // }
     }
 })
